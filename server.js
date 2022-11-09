6684b0cbd7fb35b63de801bc300470c466d13630
Arsalan <arsalan.najm.141@gmail.com>
2022-11-09 11:33:21 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const currentRoute = require('./routes/current');
+const forecastRoute = require('./routes/forecast');
+const PORT = process.env.PORT || 5000;
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(express.static(path.resolve(__dirname, './frontend/build')));
+app.use(currentRoute);
+app.use(forecastRoute);
+
+app.use('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, './frontend/build', 'index.html'));
+});
+app.listen(PORT, (err) => {
+	if (err) {
+		console.log(err);
+	}
+	console.log('app running on port', PORT);
+});

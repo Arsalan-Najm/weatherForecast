fc7c4e3fa93ffc1376771ea8479050bc260b58ed
Arsalan <arsalan.najm.141@gmail.com>
2022-11-10 15:04:35 +0100

    Add files via upload

--- routes/forecast.js
+++ routes/forecast.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/forecast', (req, res) => {
-	axios.get('https://ipapi.co/json').then((response) => {
+	axios.get('https://ipapi.co/city').then((response) => {
 		const city = response.data;
 		axios
 			.get(
-				`https://api.openweathermap.org/data/2.5/forecast?lat=${city.latitude}&lon=${city.longitude}&appid=80747e5e5919d14704c6fafc03db8015&units=metric`
+				`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=80747e5e5919d14704c6fafc03db8015&units=metric`
 			)
 			.then((response2) => {
 				res.send(response2.data.list);

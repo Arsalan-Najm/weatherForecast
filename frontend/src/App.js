9ed78c039368430908dae41bd68663edd50100b6
Arsalan <arsalan.najm.141@gmail.com>
2022-12-20 14:08:40 +0100

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,47 +3,49 @@ import Header from './components/header';
 import Search from './components/search';
 import CurrentWeather from './components/currentWeather components/current';
 import Forecast from './components/forecast components/forecast';
+import Detail from './components/details components/detail';
 import Footer from './components/footer';
 import { WeatherContext } from './context/weatherContext';
 import { ClockLoader } from 'react-spinners';
 import $ from 'jquery';
 function App() {
-	const [currentWeather, setCurrentWeather] = useState({});
-	const [forecast, setForeCast] = useState([{}]);
-	const [location, setLocation] = useState('');
-	const loaderRef = useRef();
-	useEffect(() => {
-		$(loaderRef.current).fadeOut(1500);
-	});
-	return (
-		<WeatherContext.Provider
-			value={{
-				currentWeather,
-				setCurrentWeather,
-				location,
-				setLocation,
-				forecast,
-				setForeCast,
-			}}
-		>
-			<div className='App w-full h-screen'>
-				<div
-					className='fixed w-full h-screen bg-main z-50 flex justify-center items-center'
-					ref={loaderRef}
-				>
-					<ClockLoader
-						size={70}
-						color='#f8cb58'
-					/>
-				</div>
-				<Header />
-				<Search />
-				<CurrentWeather />
-				<Forecast />
-				<Footer />
-			</div>
-		</WeatherContext.Provider>
-	);
+  const [currentWeather, setCurrentWeather] = useState({});
+  const [forecast, setForeCast] = useState([{}]);
+  const [location, setLocation] = useState('');
+  const [fahrenheit, setFahrenheit] = useState(false);
+  const loaderRef = useRef();
+  useEffect(() => {
+    $(loaderRef.current).fadeOut(1500);
+  });
+  return (
+    <WeatherContext.Provider
+      value={{
+        currentWeather,
+        setCurrentWeather,
+        location,
+        setLocation,
+        forecast,
+        setForeCast,
+        fahrenheit,
+        setFahrenheit,
+      }}
+    >
+      <div className='App w-full h-screen'>
+        <div
+          className='fixed w-full h-screen bg-main z-50 flex justify-center items-center'
+          ref={loaderRef}
+        >
+          <ClockLoader size={70} color='#f8cb58' />
+        </div>
+        <Header />
+        <Search />
+        <CurrentWeather />
+        <Forecast />
+        <Detail />
+        <Footer />
+      </div>
+    </WeatherContext.Provider>
+  );
 }
 
 export default App;

c45347be50d2fb80bf88bcc3040fceb2e1d16526
Arsalan <arsalan.najm.141@gmail.com>
2022-12-20 14:10:31 +0100

    Add files via upload

--- frontend/src/components/currentWeather components/temperature.jsx
+++ frontend/src/components/currentWeather components/temperature.jsx
@@ -2,25 +2,39 @@ import React, { useContext } from 'react';
 import { WeatherContext } from '../../context/weatherContext';
 
 function Temprature() {
-	const { currentWeather } = useContext(WeatherContext);
-	return (
-		<div className='flex justify-center'>
-			<h1 className='text-white text-lg font-semibold'>
-				H:
-				<span className='text-secondary'>
-					{currentWeather.main?.temp_max}
-					<sup>o</sup>
-				</span>
-			</h1>
-			&nbsp;&nbsp;
-			<h1 className='text-white text-lg font-semibold'>
-				L:
-				<span className='text-secondary'>
-					{currentWeather.main?.temp_min}
-					<sup>o</sup>
-				</span>
-			</h1>
-		</div>
-	);
+  const { currentWeather, fahrenheit } = useContext(WeatherContext);
+  return (
+    <div className='flex justify-center'>
+      <h1 className='text-white text-lg font-semibold'>
+        H:
+        {fahrenheit ? (
+          <span className='text-secondary'>
+            {(currentWeather.main?.temp_max.toFixed() * 9) / 5 + 32}
+            <sup>o</sup>
+          </span>
+        ) : (
+          <span className='text-secondary'>
+            {currentWeather.main?.temp_max}
+            <sup>o</sup>
+          </span>
+        )}
+      </h1>
+      &nbsp;&nbsp;
+      <h1 className='text-white text-lg font-semibold'>
+        L:
+        {fahrenheit ? (
+          <span className='text-secondary'>
+            {(currentWeather.main?.temp_min.toFixed() * 9) / 5 + 32}
+            <sup>o</sup>
+          </span>
+        ) : (
+          <span className='text-secondary'>
+            {currentWeather.main?.temp_min}
+            <sup>o</sup>
+          </span>
+        )}
+      </h1>
+    </div>
+  );
 }
 export default Temprature;
